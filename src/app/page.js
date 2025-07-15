fc6efc68be5b8f854e3fdfc219bb824d910912c2
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2025-07-15 17:10:46 +0600

    DONE1

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
           property="og:description"
           content="Protect your vehicle with SecureMyRide's expert mobile installation of Ghost immobilisers, GPS trackers, and anti-theft devices. Serving all London boroughs."
         />
-        <meta property="og:url" content="https://framer-task.vercel.app/" />
+        <meta property="og:url" content="https://framertask.vercel.app" />
       </Head>
 
       <HeroSection />
